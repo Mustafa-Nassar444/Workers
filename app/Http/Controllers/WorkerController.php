a3af21c11cd96ea7b162abfc020b7ce232d17487
Mustafa-Nassar444 <mustafanassar122@gmail.com>
2023-08-07 00:09:57 +0300

    Multi Auth

---
+++ app/Http/Controllers/WorkerController.php
@@ -0,0 +1,106 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Worker;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class WorkerController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:worker', ['except' => ['login', 'register']]);
+    }
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        if (! $token = auth()->guard('worker')->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $this->createNewToken($token);
+    }
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+            'email' => 'required|string|email|max:100|unique:clients',
+            'password' => 'required|string|min:6',
+            'phone'=>'required|string|max:20',
+            'photo'=>'required|image',
+            'location'=>'required|string'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        $worker = Worker::create(array_merge(
+            $validator->validated(),
+            [
+                'password' => bcrypt($request->password),
+                'photo'=>$request->file('photo')->store('Worker'),
+            ]
+        ));
+        return response()->json([
+            'message' => 'Worker successfully registered',
+            'user' => $worker
+        ], 201);
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout() {
+        auth()->guard('worker')->logout();
+        return response()->json(['message' => 'Worker successfully signed out']);
+    }
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh() {
+        return $this->createNewToken(auth()->guard('worker')->refresh());
+    }
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function workerProfile() {
+        return response()->json(auth()->guard('worker')->user());
+    }
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->guard('worker')->user()
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{AdminController ,ClientController, WorkerController};
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+Route::middleware('DbBackup')->prefix('auth')->group(function (){
+    Route::controller(AdminController::class)->prefix('admin')->group(
+        function () {
+            Route::post('/login','login');
+            Route::post('/register',  'register');
+            Route::post('/logout',  'logout');
+            Route::post('/refresh', 'refresh');
+            Route::get('/admin-profile', 'adminProfile');
+        });
+    Route::controller(ClientController::class)->prefix('client')->group(
+        function () {
+            Route::post('/login','login');
+            Route::post('/register',  'register');
+            Route::post('/logout',  'logout');
+            Route::post('/refresh', 'refresh');
+            Route::get('/client-profile', 'clientProfile');
+        });
+    Route::controller(WorkerController::class)->prefix('worker')->group(
+        function () {
+            Route::post('/login','login');
+            Route::post('/register',  'register');
+            Route::post('/logout',  'logout');
+            Route::post('/refresh', 'refresh');
+            Route::get('/worker-profile', 'workerProfile');
+        });
+});
+
